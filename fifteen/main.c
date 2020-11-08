4f0484d17b12348afdf3f754f4e661f830711b79
Tanya <tpikulenko@gmail.com>
2020-11-08 15:13:10 +0200

    Initial Commit

---
+++ fifteen/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fifteen
+//
+//  Created by Tatyana on 08.11.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
